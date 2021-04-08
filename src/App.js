e1a00d03d5198377b6bf115eba3b0148cf382771
roman <romantcvetkov@gmail.com>
2021-04-08 13:27:00 +0300

    60 leacture withRouter, props.match.params

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ const App = () => {
                         <Route path="/dialogs">
                             <DialogsContainer />
                         </Route>
-                        <Route path="/profile">
+                        <Route path="/profile/:userId?">
                             <ProfileContainer />
                         </Route>
                         <Route path="/users">
