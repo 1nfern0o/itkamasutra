08d35a14bc20ef5362e746f70ce10487d8687166
roman <romantcvetkov@gmail.com>
2021-04-09 10:48:02 +0300

    63 leacture - practice, DAL, axios.create

---
+++ src/api/api.js
@@ -0,0 +1,19 @@
+import * as axios from "axios";
+
+const instance = axios.create({
+    withCredentials: true,
+    baseURL: "https://social-network.samuraijs.com/api/1.0/",
+    headers: {
+        "API-KEY": "d0ce4cd8-d0d2-4152-99ad-f6e1407cb23f"
+    }
+});
+
+export const usersAPI = {
+    getUsers(currentPage = 1, pageSize = 10) {
+        return instance.get(`users?page=${currentPage}&count=${pageSize}`).then(response => response.data);
+    }
+};
+
+export const getUsers2 = (currentPage = 1, pageSize = 10) => {
+    return instance.get(`follow?page=${currentPage}&count=${pageSize}`).then(response => response.data);
+};
